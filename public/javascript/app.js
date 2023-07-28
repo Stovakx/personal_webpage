a2da962236d6ec1d141fc7805e19a8fad12552d6
Stovakx <112606493+Stovakx@users.noreply.github.com>
2023-07-28 13:32:53 +0200

    problem with bs js
    navbar doesn't show on smaller devices, app.js i have code for it.

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,14 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
     const navbarToggler = document.querySelector(".navbar-toggler");
-    const navbarTogglerIcon = navbarToggler.querySelector(".navbar-toggler-icon");
-
+    const navbarTogglerIcon = document.querySelector(".navbar-toggler-icon");
+    const collapse = document.getElementById('navbarMenu');
     navbarToggler.addEventListener("click", function () {
         if (navbarTogglerIcon.classList.contains("bi-list")) {
             navbarTogglerIcon.classList.remove("bi-list");
             navbarTogglerIcon.classList.add("bi-x");
+            collapse.classList.add('show')
         } else {
+
             navbarTogglerIcon.classList.remove("bi-x");
             navbarTogglerIcon.classList.add("bi-list");
+            collapse.classList.remove('show')
         }
+
     });
 });
