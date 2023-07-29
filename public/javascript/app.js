92604f926392484eac6d5e8b12d862a649bd26fd
Stovakx <112606493+Stovakx@users.noreply.github.com>
2023-07-29 23:26:18 +0200

    css repairs + fetch js

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
 
     });
-    
+
     //year calculating
     const yearElement = document.querySelector('.year');
     const currentYear = new Date().getFullYear();
@@ -52,5 +52,36 @@ document.addEventListener("DOMContentLoaded", function () {
         toggleScrollToTopButton();
         checkScrollDirection();
     };
+
+    document.querySelector('.btn_form_submit').addEventListener('submit', (e) => {
+        e.preventDefault();
+    });
+    // variable form for nodemailer in server
+    const form = document.getElementById('contactForm')
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+    
+      const formData = new FormData(form);
+    
+      fetch("/sendmail", {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          alert(data.message);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+          alert("There was a problem sending your message. Please try again later.");
+        });
+    });
+    
 
 });
